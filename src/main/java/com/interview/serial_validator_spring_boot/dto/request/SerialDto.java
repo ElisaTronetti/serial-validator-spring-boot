a92b9f2888c3b1b5b6570a71068e771ba6f83331
Elisa Tronetti <elisa.tronetti1@gmail.com>
2025-07-29 12:17:58 +0200

    chore: added dto constructor

--- src/main/java/com/interview/serial_validator_spring_boot/dto/request/SerialDto.java
+++ src/main/java/com/interview/serial_validator_spring_boot/dto/request/SerialDto.java
@@ -2,12 +2,14 @@ package com.interview.serial_validator_spring_boot.dto.request;
 
 import com.interview.serial_validator_spring_boot.validation.ValidChecksum;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class SerialDto {
     @Pattern(regexp = "^[A-Z][A-Za-z0-9]{15}$", message = "Checksum failed")
     @ValidChecksum
